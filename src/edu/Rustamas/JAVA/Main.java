42394b1efb71c1368dc4fd9d46cc8e02570c3590
Rustamas <rustams@live.com>
2020-10-28 23:48:13 +0200

    made some changes

--- src/edu/Rustamas/JAVA/Main.java
+++ src/edu/Rustamas/JAVA/Main.java
@@ -11,7 +11,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String expression = scanner.nextLine();
-            if (expression.equals("quit")) {
+            if (expression.equals("q")) {
                 break;
             }
             System.out.println("Result: " + calculate(expression));
